dc5c73f284a0c09b622bcce39f6e1e4481555192
Filipe Batista <filipe.x@live.com>
2020-12-22 18:24:21 -0300

    entendendo conceitos do react

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,14 @@
+import React, {useState} from 'react';
+
 function App() {
+  const [v, setV] = useState(0);
+  function adicionar(){
+    setV(v + 1);
+  }
   return (
     <div>
-      Olá!
+      Contagem {v}
+      <button onClick={adicionar}>Adicionar</button>
     </div>
   );
 }
